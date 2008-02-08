c1e111595bc0032831a97f6f3a94eeaffc9e110a
Yossef Mendelssohn <ymendel@pobox.com>
2008-02-07 23:39:41 -0600

    Added license file and filled out readme. This may be premature, but I wanted a good place to record my Chronic.parse('today') adventures.

--- spec/autochronic_spec.rb
+++ spec/autochronic_spec.rb
@@ -28,12 +28,6 @@ describe SpaceCowboy do
     end
     
     # WTF: this is presently giving me the next day
-    # >> Chronic.parse('today')
-    # => Fri Feb 08 00:00:00 -0600 2008
-    # >> Date.today.to_s
-    # => "2008-02-07"
-    # >> Time.now
-    # => Thu Feb 07 23:13:28 -0600 2008
     it 'should accept a natural language date string' do
       date = Date.today
       date_input = 'today'
