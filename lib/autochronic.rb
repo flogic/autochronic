2a6e587762339ae356a1a7d9e816b0ebcb5ca818
Yossef Mendelssohn <ymendel@pobox.com>
2008-02-07 23:56:19 -0600

    Refactoring the date manipulation in the same pattern as the time stuff.

--- lib/autochronic.rb
+++ lib/autochronic.rb
@@ -3,13 +3,14 @@ require 'chronic'
 
 class ActiveRecord::ConnectionAdapters::Column
   class << self
-    def string_to_date(string)
-      return string unless string.is_a?(String)
-      return nil if string.empty?
+    def string_to_date_with_chronic(string)
+      result = string_to_date_without_chronic(string)
+      return result if result
       
       parsed = Chronic.parse(string)
       parsed and parsed.to_date
     end
+    alias_method_chain :string_to_date, :chronic
     
     def string_to_time_with_chronic(string)
       result = string_to_time_without_chronic(string)
